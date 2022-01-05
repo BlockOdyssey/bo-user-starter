925cd30121a517cf21b720ae9d5d5544f03f46af
blockodyssey-kjlee <kukjin@blockodyssey.io>
2022-01-05 14:48:10 +0900

    복수의 요청일때 토큰 재발행 로직 수정

--- default/src/api/instance.js
+++ default/src/api/instance.js
@@ -8,11 +8,17 @@ const instance = axios.create({
     timeout: 5000
 });
 
+let isTokenRefreshing = false;
+let refreshSubscribers = [];
+
+const onTokenRefreshed = (accessToken) => refreshSubscribers.map((callback) => callback(accessToken));
+const addRefreshSubscriber = (callback) => refreshSubscribers.push(callback);
+
 instance.defaults.headers.common["Content-Type"] = "application/json; charset=UTF-8";
 instance.defaults.headers.common["Accept"] = "application/json";
 // instance.defaults.withCredentials = true; 토큰방식이 아닌 세션을 사용할 경우
 instance.interceptors.request.use((config) => {
-    if (config.tokenFlag === true) config.headers["token"] = _getAccessToken();
+    if (config.tokenFlag === true) config.headers.Authorization = `Bearer ${_getAccessToken()}`;
     if (config.fileFlag === true) config.headers["Content-Type"] = "multipart/form-data";
 
     delete config.tokenFlag;
@@ -28,35 +34,48 @@ instance.interceptors.response.use(
     (err) => {
         const originalRequest = err.config;
 
-        if (err.response?.status === 401 && !originalRequest._retry) {
-            // 무한반복을 피하기 위한 로직
-            originalRequest._retry = true;
+        if (err.response?.status === 401) {
+            if (isTokenRefreshing === false) {
+                isTokenRefreshing = true;
 
-            // 토큰 갱신 로직 실행
-            return axios
-                .post("/auth/token", {
-                    refresh_token: _getRefreshToken()
-                })
-                .then(async (res) => {
-                    if (res.status === 201) {
-                        // 갱신된 토큰을 입력
-                        _setToken(res.data);
+                // 토큰 갱신 로직 실행
+                return axios
+                    .post("/auth/token", {
+                        refresh_token: _getRefreshToken()
+                    })
+                    .then(async (res) => {
+                        if (res.status === 201) {
+                            // 갱신된 토큰을 입력
+                            _setToken(res.data);
 
-                        // header에다가 갱신된 토큰을 다시 입력
-                        originalRequest.headers.token = _getAccessToken();
+                            // header에다가 갱신된 토큰을 다시 입력
+                            let accessToken = _getAccessToken();
 
-                        // 재요청
-                        return axios(originalRequest);
-                    }
-                })
-                .catch((error) => {
-                    // refresh 토큰도 만료됐을 경우 에러가 날 것으로 추정됨.
-                    console.log(error);
-                    // 토큰 초기화
-                    _clearToken();
+                            isTokenRefreshing = false;
 
-                    // 개발할때 로그인 페이지로 이동하는 방법에 대해서 구현해야 함.
+                            // 저장된 API 요청에 새로운 함수를 주면서 재요청
+                            await onTokenRefreshed(accessToken);
+
+                            refreshSubscribers = [];
+                        }
+                    })
+                    .catch((error) => {
+                        // refresh 토큰도 만료됐을 경우 에러가 날 것으로 추정됨.
+                        console.log(error);
+                        // 토큰 초기화
+                        _clearToken();
+
+                        // 개발할때 로그인 페이지로 이동하는 방법에 대해서 구현해야 함.
+                    });
+            }
+            // token이 재발급 되는 동안 API 요청은 refreshSubscribers에 저장
+            const retryOriginalRequest = new Promise((resolve) => {
+                addRefreshSubscriber((accessToken) => {
+                    originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+                    resolve(axios(originalRequest));
                 });
+            });
+            return retryOriginalRequest;
         } else {
             return Promise.reject({ statusCode: err.response?.status || 500, message: err.response?.status ? err.message : "네트워크 에러" });
         }
